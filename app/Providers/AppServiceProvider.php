9f785e9769b88c5afa81ec20bb5a0f8c47126756
Топалов Василий <topalek@mail.ru>
2025-04-04 12:44:02 +0300

    stubs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict(!app()->isProduction());
+        Model::unguard();
+        DB::prohibitDestructiveCommands(!app()->isProduction());
     }
 }
